6e092ad3927fa802e0bfa80a6c532a302e823ff8
sameer <samwdx@gmail.com>
2020-02-04 20:32:52 -0500

    uwu

---
+++ tictactoerules.java
@@ -0,0 +1,20 @@
+public class tictactoerules {
+
+    // BingoCard Application
+    public static void main(String[] args) {
+
+        System.out.println("Tic Tac Toe Rules\n");
+        System.out.println("Tic Tac Toe is a game for 2 players.\n");
+        System.out.println("1. A grid of 9 squares is drawn.\n");
+        System.out.println("2. The first player draws an X in an empty square.\n");
+        System.out.println("3. The second player draws an O in a empty square.\n");
+        System.out.println("4. Steps 2 and 3 are repeated until a Tic Tac Pattern is created:.\n");
+        System.out.println("\tPatterns can be diagonal, horizontal, or vertical.\n");
+        System.out.println("5. A winning pattern is completed by drawing a line through the pattern.\n");
+
+
+
+
+    }
+
+}
