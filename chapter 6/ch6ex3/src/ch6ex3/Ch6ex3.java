a767336a7c79ab7e4340024e263f0181a834c029
sameer <samwdx@gmail.com>
2020-02-19 09:47:11 -0500

    www

---
+++ chapter 6/ch6ex3/src/ch6ex3/Ch6ex3.java
@@ -0,0 +1,32 @@
+package ch6ex3;
+import java.lang.*;
+/**
+ *
+ * @author swaheed9387
+ */
+public class Ch6ex3 {
+
+    public static void main(String[] args) {
+     double r = 0.075;
+     double f;
+     int y = 0;
+    
+     for (double p=2500;p<=5000;y++) {
+    
+     f = p*Math.pow(1+(r/12),y);
+     
+
+     
+     }
+     
+    
+     
+     
+   
+        
+    
+    
+
+    }
+
+}
